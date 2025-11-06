c04e9d0bf2c24c2ee5df4461775b182538548e4b
pedrosaura <psauraf@gmail.com>
2025-11-06 10:49:06 +0100

    Finish challenge06

---
+++ challenge06/starter.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int suma(int a, int b);
+
+#ifndef UNIT_TEST
+int main(void) {
+    printf("Pipeline local simulado.\n");
+    return 0;
+}
+#endif
---
+++ challenge06/test_challenge06.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <stdio.h>
+
+int suma(int a, int b);
+
+int suma(int a, int b) {
+    return a + b;
+}
+
+int main(void) {
+    assert(suma(2, 3) == 5);
+    assert(suma(-1, 1) == 0);
+    printf("✅ Todas las pruebas pasaron correctamente.\n");
+    return 0;
+}
